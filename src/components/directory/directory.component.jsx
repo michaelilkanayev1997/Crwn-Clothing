1907a7b92baabc8c080e43a2c5e176d0257b630b
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2022-10-14 16:37:05 +0300

    Font update

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import CategoryItem from "../category-item/category-item.component";
 
 const Directory = ({ categories }) => {
 
-    return (
+    return ( //directory 
         <div className="directory-container">
           {categories.map((category) => (
             <CategoryItem key={category.id} category={category} />
