c553fd511339f6d5e76bd47f82a4a448ac0c3ecf
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-02 22:45:29 +0300

    Stripe payment update + netlify serverless function

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  "pk_test_51NacF6LWmKGLUv5iuZuLfzpRJBRYEsnYBkR9IBLkA1X6Vmkhpm162f8CQn3qwPA2m8SZE0UaYsjx5vWcfjgu7vCD007K70Rv42"
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
